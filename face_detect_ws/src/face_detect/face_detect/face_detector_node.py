15b1074316ffe3796a846e01285643c57b772720
lifeofsankar <jaisankarjaikishan369@gmail.com>
2025-08-08 14:06:39 +0530

    Started working on Face Detection

--- face_detect_ws/src/face_detect/face_detect/face_detector_node.py
+++ face_detect_ws/src/face_detect/face_detect/face_detector_node.py
@@ -5,3 +5,9 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from visualization_msgs.msg import Marker
 from cv_bridge import CvBridge
+
+
+class FaceDetectNode(Node):
+    def __init__(self):
+        super().__init__('face_detect_node')
+        
